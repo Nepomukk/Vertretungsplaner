1cf9c2d62d505af1f3502f32ceca378411872742
Merlin2092 <merlin2092@gmx.de>
2022-11-14 16:23:45 +0100

    Add basic menu

--- frontend/main.py
+++ frontend/main.py
@@ -11,10 +11,19 @@ app.config["DEBUG"] = True
 # load all file names from the respective folder to automatically add all files within
 js_files = os.listdir('static/js')
 css_files = os.listdir('static/css')
+
+menu_items = {
+    'formular': {
+        'name': 'Formular erstellen',
+        'path': '/formular',
+    },
+}
+
 # add to default var set for all templates
 default = {
     "js_files": js_files,
     "css_files": css_files,
+    "menu_items": menu_items,
 }
 
 
@@ -22,6 +31,12 @@ default = {
 def home():
     name = 'Lehrer x'
     return render_template('pages/home.html', default=default, username=name)
+
+
+@app.route('/formular')
+def formular_page():
+    name = 'Lehrer x'
+    return render_template('pages/formular.html', default=default, username=name)
 
 
 app.run()
