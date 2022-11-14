f18d94c19450d77bedc1fc6360bf2e518f42b48f
Merlin2092 <merlin2092@gmx.de>
2022-11-14 13:02:00 +0100

    Change file structure

--- frontend/main.py
+++ frontend/main.py
@@ -3,16 +3,14 @@ import flask
 from flask import request, jsonify
 from flask import render_template
 
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    name = 'Rosalia'
-    return render_template('home.html', username=name)
+def home():
+    name = 'Lehrer x'
+    return render_template('pages/home.html', username=name)
 
 
 app.run()
