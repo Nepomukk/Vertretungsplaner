fd5637d6880ebaf615c852f5bbdf3197518decae
user <christiane.m@gso.schule.koeln>
2022-11-15 12:09:28 +0100

    code corrections

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,11 +1,9 @@
-
-
 function showMessages(){
 
     var toggleBox = document.getElementById('toggleBox')
     if(toggleBox.classList.contains('hidden'))
     {
-    toggleBox.classList.remove('hidden')
+    toggleBox.classList.remove('hidden')}
     else
     {
     toggleBox.classList.add('hidden')
