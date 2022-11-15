e2efd009e0c5e1e13b328f4a108c5904a247e687
user <christiane.m@gso.schule.koeln>
2022-11-15 12:05:36 +0100

    change showMessages function, add hidden class mnaually as bootstrap class hidden does not work

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -3,5 +3,11 @@
 function showMessages(){
 
     var toggleBox = document.getElementById('toggleBox')
+    if(toggleBox.classList.contains('hidden'))
+    {
     toggleBox.classList.remove('hidden')
+    else
+    {
+    toggleBox.classList.add('hidden')
+    }
 }
