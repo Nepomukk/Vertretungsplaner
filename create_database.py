567a0cf2c9a703b8ba7924fbe04ce4467e36dcdb
talarias <talarias@live.de>
2022-11-16 16:59:26 +0100

    refactoring

---
+++ create_database.py
@@ -0,0 +1,13 @@
+import inspect
+import os
+import sys
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from database.dbHelper import Session
+
+
+if __name__ == '__main__':
+    Session.createDatabase()
